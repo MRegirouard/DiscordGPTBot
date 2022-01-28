edf30f390cd886ebe204d238d689f4a8c711676c
Ethan Girouard <mr.egirouard@gmail.com>
2022-01-28 13:46:45 -0500

    Add config file reading

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ const log = require('./customLog.js')
 log.info('')
 log.info('Starting Discord GPT Bot')
 log.info('')
+
+log.debug('Loading config file reader package...')
+
+const configReader = require('@eta357/config-reader')
+
+log.verbose('Loaded config file reader package.')
+
+const configFile = "config.json"
+var config
+const configOptions = 
+{
+    'Discord API Token':''
+}
+
+log.verbose('Reading config file...')
+
+configReader.readOptions(configFile, configOptions, false, true, true, 2).then((result) =>
+{
+    log.info('Config file read successfully.')
+    config = result
+})
+.catch((err) =>
+{
+    log.error('Error reading config file: ' + err)
+    log.error('Exiting with exit code 1.')
+    process.exit(1)
+})
